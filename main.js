39ec63a6184d99aa46b0616cc7ea58ebb1da43d2
Raj <singhraj1553@outlook.com>
2023-01-16 00:11:59 +0530

    Initial files

---
+++ main.js
@@ -0,0 +1,34 @@
+// Get elements from DOM
+const modalBtn=document.querySelector('.yt-modal-btn');
+const modal=document.querySelector('.modal-bg');
+
+// Add click event to the button
+modalBtn.addEventListener('click', ()=>{
+    // Add youtube embed to the HTML
+
+    // You can get the embed code by right clcicking any youtube video and selecting 'ember code'
+
+    modal.innerHTML=`<div class="modal">
+    <iframe width="892" height="502" src="https://www.youtube.com/embed/hrRvFS1qIAQ" title="Jibaro | Love, Death and Robots" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>`;
+    // Show the modal
+    modal.style.display="flex";
+    // Smooth opacity transition
+    setTimeout(()=>{
+        modal.style.opacity="1";
+    },50);
+});
+
+// Add click event to the modal bg
+modal.addEventListener('click', ()=>{
+    // Remove youtube ember to the HTML
+
+    // This is a simple way to stop the video from playing from playing when you close the modal
+    modal.innerHTML='';
+    // smooth opacity transition
+    modal.style.opacity="0";
+    setTimeout(()=>{
+        // Hide the modal when the transition is done
+        modal.style.display="none";
+    },450);
+});
